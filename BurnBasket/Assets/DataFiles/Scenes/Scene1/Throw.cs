8c1fbb0005f86a057c5c8884048958e27673933d
Kartik Kinge <kingekartik@gmail.com>
2018-05-30 15:12:30 +0530

    AR Scene
    AR Scene has been added.
    Folders have been organised.
    Keep the developement up on your own scenes later the features can be clubbed with some edits.

---
+++ BurnBasket/Assets/DataFiles/Scenes/Scene1/Throw.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour {
+
+	public float throwSpeed = 10;
+	float speed = 0;
+	
+	void Update () {
+		speed = Mathf.Lerp (speed, 0, Time.deltaTime);
+		transform.Translate(Vector3.forward * speed);
+		if (Input.GetMouseButtonDown (0))
+			speed = throwSpeed;
+	}	
+}
